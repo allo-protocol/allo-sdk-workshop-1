8b89e734a0fc2af01a78186ba1b816c0eb939d6a
Jaxcoder <codenamejason@outlook.com>
2023-12-11 21:22:28 -0500

    update links

--- starter/src/components/Home.tsx
+++ starter/src/components/Home.tsx
@@ -7,6 +7,7 @@ import {
     midnightTheme,
 } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
+import Link from "next/link";
 import { WagmiConfig, configureChains, createConfig } from "wagmi";
 import { arbitrum, base, mainnet, optimism, zora } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
@@ -52,9 +53,9 @@ const Home = () => {
             Allo
           </div>
 
-          <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3">
+            <Link
+              href="https://docs.allo.gitcoin.co/"
               className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
               target="_blank"
               rel="noopener noreferrer"
@@ -66,18 +67,18 @@ const Home = () => {
                 </span>
               </h2>
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Find in-depth information about Next.js features and API.
+                Find in-depth information about Allo .
               </p>
-            </a>
+            </Link>
 
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            <Link
+              href="https://github.com/allo-protocol/allo-v2-sdk"
               className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
               target="_blank"
               rel="noopener noreferrer"
             >
               <h2 className={`mb-3 text-2xl font-semibold`}>
-                Learn{" "}
+                SDK Github{" "}
                 <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                   -&gt;
                 </span>
@@ -85,16 +86,16 @@ const Home = () => {
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                 Learn about Next.js in an interactive course with&nbsp;quizzes!
               </p>
-            </a>
+            </Link>
 
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            <Link
+              href="https://github.com/allo-protocol/allo-v2"
               className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
               target="_blank"
               rel="noopener noreferrer"
             >
               <h2 className={`mb-3 text-2xl font-semibold`}>
-                Templates{" "}
+                Allo Github{" "}
                 <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                   -&gt;
                 </span>
@@ -102,25 +103,7 @@ const Home = () => {
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                 Explore starter templates for Next.js.
               </p>
-            </a>
-
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                Deploy{" "}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                  -&gt;
-                </span>
-              </h2>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Instantly deploy your Next.js site to a shareable URL with
-                Vercel.
-              </p>
-            </a>
+            </Link>
           </div>
         </main>
       </RainbowKitProvider>
